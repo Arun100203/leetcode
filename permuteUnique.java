5ceeccec7fc0dba8a990dc5fae2845cf9082f2f6
ARUN L <108601293+Arun100203@users.noreply.github.com>
2023-03-02 05:48:27 +0530

    Create permuteUnique.java

---
+++ permuteUnique.java
@@ -0,0 +1,32 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    Set<List<Integer>> set = new HashSet<>();
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        permutations(nums, 0);
+        for(List<Integer> l : set)
+        ans.add(l);
+        return ans;
+    }
+    void permutations(int nums[], int l)
+    {
+        if(l==nums.length-1)
+        {
+            List<Integer> li = new ArrayList<>();
+            for(int i:nums)
+            li.add(i);
+            set.add(li);
+        }
+        for(int i=l;i<nums.length;i++)
+        {
+            swap(nums, l, i);
+            permutations(nums, l+1);
+            swap(nums, l, i);
+        }
+    }
+    void swap(int nums[], int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
