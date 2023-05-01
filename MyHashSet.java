6e376d0ddbe47b7c473138802bb6606aaa4a767d
ARUN L <108601293+Arun100203@users.noreply.github.com>
2023-05-01 16:27:42 +0530

    Create MyHashSet.java

---
+++ MyHashSet.java
@@ -0,0 +1,26 @@
+class MyHashSet {
+    boolean set[];
+    public MyHashSet() {
+        set = new boolean[1000001];
+    }
+    
+    public void add(int key) {
+        set[key] = true;
+    }
+    
+    public void remove(int key) {
+        set[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
