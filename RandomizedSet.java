87eca4c943c5c719b9a25af5acaa6ea5e4773f34
ARUN L <108601293+Arun100203@users.noreply.github.com>
2023-05-01 16:29:59 +0530

    Create RandomizedSet.java

---
+++ RandomizedSet.java
@@ -0,0 +1,36 @@
+class RandomizedSet {
+    HashSet<Integer> set;
+    public RandomizedSet() {
+        set = new HashSet<>();
+    }
+    
+    public boolean insert(int val) {
+        if(set.contains(val)){
+            return false;
+        }
+        set.add(val);
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if(set.contains(val)){
+            set.remove(val);
+            return true;
+        }
+        return false;
+    }
+    
+    public int getRandom() {
+        ArrayList<Integer> arr = new ArrayList<>(set);
+        int random =  (int)(Math.random()*set.size());
+        return arr.get(random);
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
